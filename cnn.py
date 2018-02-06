2ef004610507d34af4cabe1fb9117d789503d5dc
unknown <ken.murrell13@gmail.com>
2018-02-05 19:37:23 -0500

    quick update

--- cnn.py
+++ cnn.py
@@ -1,6 +1,13 @@
 import tensorflow as tf
 import numpy as np
 
+POS_DATASET_PATH = 'tw-data.pos'
+NEG_DATASET_PATH = 'tw-data.neg'
+VOC_PATH = 'vocab.csv'
+VOC_INV_PATH = 'vocab_inv.csv'
+
+build
+
 # Load data
 x, y, vocabulary, vocabulary_inv = load_data(FLAGS.dataset_fraction)
 
@@ -9,3 +16,9 @@ np.random.seed(123)
 shuffle_indices = np.random.permutation(np.arange(len(y)))
 x_shuffled = x[shuffle_indices]
 y_shuffled = y[shuffle_indices]
+
+def prep_data():
+    voc = csv.reader(open(VOC_PATH))
+    voc_inv = csv.reader(open(VOC_INV_PATH))
+    vocabulary_inv = [x for x in voc_inv]
+    vocabulary = {x: i for x, i in voc}
